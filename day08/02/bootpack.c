e45aaf8662b3f8d59ef03983c1618b884a040884
Sirlsliang <648767807@qq.com>
2016-12-02 22:31:27 +0800

    add review

--- day08/02/bootpack.c
+++ day08/02/bootpack.c
@@ -66,11 +66,48 @@ void HariMain(void){
 			}else if (fifo8_status(&mousefifo) != 0){
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s, "%02X", i);
-				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
-			}
-		} 
+				if(mouse_decode(&mdec, i) != 0){
+					/* 鼠标的三个字节都已经收集齐了,将他们显示出来 */
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if((mdec.btn & 0x01) != 0){ //	判断最低位是否为1
+						s[1] ='L';
+					}
+					if((mdec.btn & 0x02) != 0){	//	判断中间位是否为1
+						s[3] ='R';
+					}
+					if((mdec.btn & 0x04) != 0){	//	判断最高位是否为1
+						s[2] ='C';
+					}
+					// 清除原有位置的信息
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 -1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+					/* 鼠标移动 */
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx + 15, my + 15); // 隐藏鼠标
+					mx += mdec.x;
+					my += mdec.y;
+
+					if(mx < 0){
+						mx = 0;
+					}
+					if(my<0){
+						my = 0;
+					}
+					if(mx > binfo->scrnx - 16){
+						mx =  binfo->scrnx - 16;
+					}
+					if(my > binfo->scrny - 16){
+						my =	binfo->scrny - 16;
+					}
+					sprintf(s,"(%3d, %3d)", mx, my);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 79,15);
+					// 显示坐标
+					putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+					// 描绘鼠标
+					putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+				}
+			} 
+		}
 	}
 }
 
