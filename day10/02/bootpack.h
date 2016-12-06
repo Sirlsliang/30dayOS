7c16bf0f485f2f983896e61bf25e0de850750668
Sirlsliang <648767807@qq.com>
2016-12-06 10:48:18 +0800

    day10 finished

--- day10/02/bootpack.h
+++ day10/02/bootpack.h
@@ -172,10 +172,10 @@ struct SHTCTL{
 	struct SHEET sheets0[MAX_SHEETS];	//	存放图层信息
 };
 
+struct SHTCTL* shtctl_init(struct MEMMAN * memman, unsigned char* vram,int xsize, int ysize);
+struct SHEET* sheet_alloc(struct SHTCTL* ctl);
 void sheet_refresh(struct SHTCTL* ctl, struct SHEET* sht, int bx0, int by0, int bx1, int by1);
-void sheet_refreshsub(struct SHTCTL* ctl, int vx0, int vy0, int vx1, int vy1);
 void sheet_free(struct SHTCTL* ctl, struct SHEET* sht);
 void sheet_updown(struct SHTCTL* ctl, struct SHEET* sht, int height);
 void sheet_slide(struct SHTCTL* ctl, struct SHEET* sht, int vx0, int vy0);
 void sheet_setbuf(struct SHEET* sht, unsigned char* buf, int xsize, int ysize, int col_inv);
-struct SHTCTL* shtctl_init(struct MEMMAN * memman, unsigned char* vram,int xsize, int ysize);
