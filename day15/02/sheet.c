47dc175979afd2818d6c5dbe66ea526fe2abf1cd
Sirlsliang <648767807@qq.com>
2016-12-10 19:14:10 +0800

    day15 finished

---
+++ day15/02/sheet.c
@@ -0,0 +1,224 @@
+#include "bootpack.h"
+void sheet_refreshsub(struct SHTCTL* ctl, int vx0, int vy0,
+													int vx1, int vy1, int h0, int h1);
+
+struct SHTCTL* shtctl_init(struct MEMMAN * memman, unsigned char* vram,int xsize, int ysize){
+	struct SHTCTL* ctl;
+	int i;
+	ctl = (struct SHTCTL*) memman_alloc_4k(memman, sizeof(struct SHTCTL));
+	if (ctl == 0){
+		goto err;
+	}
+	ctl->map = (unsigned char*) memman_alloc_4k(memman, xsize * ysize);
+	if (ctl->map == 0){
+		memman_free_4k(memman, (int)ctl, sizeof(struct SHTCTL));
+		goto err;
+	}
+	ctl->vram 	= vram;
+	ctl->xsize 	= xsize;
+	ctl->ysize 	= ysize;
+	ctl->top		= -1;	// 没有sheets
+	for(i=0; i < MAX_SHEETS; i++){
+		ctl->sheets0[i].flags = 0; // 标记为未使用
+		ctl->sheets0[i].ctl = ctl; // 记录属性
+	}
+err:
+	return ctl;
+}
+
+// 取得新生成的未使用图层
+#define SHEET_USE	1
+struct SHEET	*sheet_alloc(struct SHTCTL* ctl){
+	struct SHEET*	sht;
+	int i;
+	for(i = 0; i < MAX_SHEETS; i++){
+		if(ctl->sheets0[i].flags == 0){
+			sht = &ctl->sheets0[i]; // 取 ctl->sheets0[i]的地址
+			sht->flags = SHEET_USE; // 标记为正在使用
+			sht->height = -1;	//隐藏
+			return sht;
+		}
+	}
+	return 0;	//	所有的SHEET都处于正在使用状态
+}
+
+// 设置绘制的图形及其属性(尺寸、颜色)
+void sheet_setbuf(struct SHEET* sht, unsigned char* buf, int xsize, int ysize, int col_inv){
+	sht->buf = buf;
+	sht->bxsize = xsize;
+	sht->bysize = ysize;
+	sht->col_inv = col_inv;
+	return;
+}
+
+//	设置图形的高度
+void sheet_updown(struct SHEET* sht, int height){
+	struct SHTCTL* ctl = sht->ctl;
+	int h, old = sht->height;	// 存储设置前的高度值(图层前后的高度，不是屏幕高度)
+
+	if (height > ctl->top + 1){
+		height = ctl->top + 1;
+	}
+	//	不显示图层
+	if (height < -1){
+		height = -1;
+	}
+	sht->height = height; // 设置图层高度
+
+	// 对sheets0 重新排列
+	if (old > height){	// 比以前低
+		if (height >= 0){
+			// 把中间的往上提
+			for (h = old; h > height; h--){
+				ctl->sheets[h] = ctl->sheets[h - 1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = h;
+			sheet_refreshmap(ctl, sht->vx0, sht->vy0,
+					sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1);
+			sheet_refreshsub(ctl, sht->vx0, sht->vy0, 
+					sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height + 1, old);
+		} else { // 隐藏
+			if (ctl->top > old){
+				// 将上面的降下来
+				for (h = old; h < ctl->top; h++){
+					ctl->sheets[h] = ctl->sheets[h + 1];
+					ctl->sheets[h]->height = h;
+				}
+			}
+			ctl->top --; 					// 由于显示中的图层少了一个，所以最上面的图层高度下降
+		}
+		sheet_refreshmap(ctl, sht->vx0, sht->vy0, 
+				sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, 
+											sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, 0, old - 1);
+	} else if (old < height){	// 比以前高 
+		if (old >= 0){					// 把中间的拉下去
+			for(h = old; h < height; h++){
+				ctl->sheets[h] = ctl->sheets[h+1];
+				ctl->sheets[h]->height = h;
+			}
+			ctl->sheets[height] = sht;
+		}else {	// 由隐藏态转为显示态。将已在上面的提上来
+			for (h = ctl->top; h >= height; h--){
+				ctl->sheets[h + 1] = ctl->sheets[h];
+				ctl->sheets[h + 1]->height = h + 1;
+			}
+			ctl->sheets[height] = sht;
+			// 由于已显示的图层增加了1个，所以最上面的图层高度增加
+			ctl->top++;
+		}
+		sheet_refreshmap(ctl, sht->vx0, sht->vy0, 
+				sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height);
+		sheet_refreshsub(ctl, sht->vx0, sht->vy0, 
+											sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize, height, height); // 按新图层信息重新绘制画面
+	}
+	return;
+}
+
+// 图层刷新
+void sheet_refresh(struct SHEET* sht, int bx0, int by0, int bx1, int by1){
+	if (sht->height >= 0){
+		sheet_refreshsub(sht->ctl, sht->vx0 + bx0, sht->vy0 + by0, 
+					sht->vx0 + bx1, sht-> vy0 + by1,	sht->height, sht->height);
+	}
+	return;
+}
+
+// 释放图层内存
+void sheet_free(struct SHEET* sht){
+	if (sht->height >= 0){
+		sheet_updown(sht, -1); // 如果处于显示状态，先设定为隐藏
+	}
+	sht->flags = 0; // 设定未使用标志
+	return;
+}
+
+// 图层移动，让图层上下左右移动
+void sheet_slide(struct SHEET* sht, int vx0, int vy0){
+	int old_vx0 = sht->vx0, old_vy0 = sht->vy0;
+	sht->vx0 = vx0;
+	sht->vy0 = vy0;
+	if (sht->height >=0){ // 如果图层正在显示
+		sheet_refreshmap(sht->ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0);
+		sheet_refreshmap(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height);
+		sheet_refreshsub(sht->ctl, old_vx0, old_vy0, 
+				old_vx0 + sht->bxsize, old_vy0 + sht->bysize, 0, sht->height - 1);
+		sheet_refreshsub(sht->ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize, sht->height,
+				sht->height);
+	}
+	return;
+}
+
+// 局部刷新
+void sheet_refreshsub(struct SHTCTL* ctl, int vx0, int vy0,
+													int vx1, int vy1, int h0, int h1){
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, c, *vram = ctl->vram, *map = ctl->map, sid;
+	struct SHEET *sht;
+	// refresh 的范围超出了画面则修正
+	if(vx0 < 0) { vx0 = 0; }
+	if(vy0 < 0) { vy0 = 0; }
+	if(vx1 > ctl->xsize) { vx1 = ctl->xsize; }
+	if(vy1 > ctl->ysize) { vy1 = ctl->ysize; }
+
+	for (h = h0; h <= h1; h++){
+		sht = ctl->sheets[h];
+		buf = sht->buf;
+
+		sid = sht - ctl->sheets0;
+		// 利用vx0 - vy1，对bx0-by1倒推
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0; 
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0; //  判断刷新处与图层的交集
+
+		if(bx0 < 0){ bx0 = 0; }
+		if(by0 < 0){ by0 = 0; } 
+		if(bx1 > sht->bxsize){ bx1 = sht->bxsize; }
+		if(by1 > sht->bysize){ by1 = sht->bysize; }
+		for (by = by0; by < by1; by++){
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++){
+				vx = sht->vx0 + bx;
+				if (map[vy * ctl->xsize + vx] == sid){
+					vram[vy * ctl->xsize + vx] = buf[by * sht->bxsize + bx];
+				}
+			}
+		}
+	}
+	return;
+}
+
+void sheet_refreshmap(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1, int h0){
+	int h, bx, by, vx, vy, bx0, by0, bx1, by1;
+	unsigned char *buf, sid, *map = ctl->map;
+	struct SHEET *sht;
+	if (vx0 < 0) { vx0 = 0; }
+	if (vy0 < 0) { vy0 = 0; }
+	if (vx1 > ctl->xsize) { vx1 = ctl->xsize; }
+	if (vy1 > ctl->ysize) { vy1 = ctl->ysize; }
+	for (h = h0; h <= ctl->top; h++){
+		sht = ctl-> sheets[h];
+		sid = sht - ctl->sheets0; // 将进行了减法计算的地址作为图层号码使用
+		buf = sht->buf;
+		bx0 = vx0 - sht->vx0;
+		by0 = vy0 - sht->vy0;
+		bx1 = vx1 - sht->vx0;
+		by1 = vy1 - sht->vy0;
+		if (bx0 < 0){ bx0 = 0; }
+		if (by0 < 0){ by0 = 0; }
+		if (bx1 > sht->bxsize) { bx1 = sht->bxsize; }
+		if (by1 > sht->bysize) { by1 = sht->bysize; }
+		for (by = by0; by < by1; by++){
+			vy = sht->vy0 + by;
+			for (bx = bx0; bx < bx1; bx++){
+				vx = sht->vx0 + bx;
+				if (buf[by * sht->bxsize + bx] != sht->col_inv){
+					map[vy * ctl->xsize + vx] = sid;
+				}
+			}
+		}
+	}
+	return;
+}
---
+++ day15/03/timer.c
@@ -0,0 +1,122 @@
+#include "bootpack.h"
+
+#define PIT_CTRL	0x0043
+#define	PIT_CNT0	0x0040
+
+struct TIMERCTL timerctl;
+
+#define TIMER_FLAGS_ALLOC	1	// 已配置状态
+#define	TIMER_FLAGS_USING	2	// 定时器运行中
+
+// PIT的初始化
+void init_pit(void){
+	int i;
+	struct TIMER *t;
+	io_out8(PIT_CTRL, 0x34);
+	io_out8(PIT_CNT0,	0x9c);
+	io_out8(PIT_CNT0, 0x2e); //0x2e9c=11932,在该数字下，中断频率为100Hz，即1秒钟会有100次中断
+	timerctl.count = 0;
+	for (i = 0; i < MAX_TIMER; i++){
+		timerctl.timers0[i].flags = 0; //未使用
+	}
+	t = timer_alloc();
+	t->timeout = 0xffffffff;
+	t->flags = TIMER_FLAGS_USING;
+	t->next_timer = 0; // 末尾
+	timerctl.t0 = t; // 只有哨兵自己
+	timerctl.next_time =  0xffffffff; //  因为只有哨兵，所以下一个超时时刻就是哨兵的时刻
+	return;
+}
+
+// 获取一个未使用的计时器
+struct TIMER *timer_alloc(void){
+	int i;
+	for (i = 0; i < MAX_TIMER; i++) {
+		if (timerctl.timers0[i].flags == 0) {
+			timerctl.timers0[i].flags = TIMER_FLAGS_ALLOC;
+			return &timerctl.timers0[i];
+		}
+	}
+	return 0;	// 未找到
+}
+
+//	释放计时器
+void timer_free(struct TIMER *timer){
+	timer->flags = 0; //未使用
+	return;
+}
+
+// 初始化计时器
+void timer_init(struct TIMER *timer, struct FIFO32 *fifo, int data){
+	timer->fifo = fifo;
+	timer->data = data;
+	return;
+}
+
+// 为计时器设置超时时间
+void timer_settime(struct TIMER *timer, unsigned int timeout){
+	int e;
+	struct TIMER *t, *s;
+	timer->timeout = timeout + timerctl.count;
+	timer->flags = TIMER_FLAGS_USING;
+	e = io_load_eflags();
+	io_cli();
+	t = timerctl.t0;
+
+	if (timer->timeout <= t->timeout){
+		// 插入最前面的情况
+		timerctl.t0 = timer;
+		timer->next_timer = t;
+		timerctl.next_time = timer->timeout;
+		io_store_eflags(e);
+		return;
+	}
+	// 寻找插入位置
+	for (;;){
+		s = t;
+		t = t->next_timer;
+		if (timer->timeout <= t->timeout){
+			// 插入到s和t之间时
+			s->next_timer = timer;
+			timer->next_timer = t;
+			io_store_eflags(e);
+			return;
+		}
+	}
+}
+
+// IRQ0发生时所调用的中断处理程序，每秒钟100次
+void inthandler20(int *esp){
+	struct TIMER *timer;
+	char ts = 0;
+	io_out8(PIC0_OCW2, 0x60); // 把IRQ-00信号接收完了的信息通知给PIC
+	timerctl.count ++;
+	if (timerctl.next_time > timerctl.count){ // 比较是否到了下一个
+		return;
+	}
+	timer = timerctl.t0;	// 将最前面的地址赋给timer
+	for (;;){
+			// timers 的定时器都处于动作中，所以不确认flags
+		if (timer->timeout > timerctl.count){
+			// timers中的定时器是根据timeout由小到大的顺序排序的，假如第一个没有到达时间
+			// 则直接跳过
+			break;
+		}
+		// 超时
+		timer->flags = TIMER_FLAGS_ALLOC;
+		if (timer != mt_timer) {
+			fifo32_put(timer->fifo, timer->data);
+		} else{
+			ts = 1; // mt_timer超时
+		}
+		timer = timer->next_timer; // 下一个定时器的地址
+	}
+	// 新移位
+	timerctl.t0 = timer;
+	//	timerctl.next 的设定
+	timerctl.next_time = timer->timeout;
+	if (ts != 0){
+		mt_taskswitch();
+	}
+	return;
+}
